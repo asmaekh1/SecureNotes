bcb4f59efcd229d240dbf54a75471cd16473c8d0
asmae.khariji <asmae.khariji@inetum.com>
2024-11-19 12:38:21 +0100

    notes project

--- src/main/java/com/secure/notes/models/Note.java
+++ src/main/java/com/secure/notes/models/Note.java
@@ -8,8 +8,10 @@ public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // used to generate unique  identifier for the note
+    @Column(name = "")
     private String ownerUsername;
     @Lob // Lob is marks an attribute as a large object, which means the attribute will be stored in a database as either BLOB (Binary Large Object)
+    @Column(name = "")
     private String content;
 
 
--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -2,24 +2,36 @@ package com.secure.notes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        //http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
+
+/*public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
                     requests
-                            .requestMatchers("/contact").permitAll()   // allow user to access the landing page without authentification
-                            .requestMatchers("/public/**").permitAll()  // allow any endpoint start with public
-                            .requestMatchers("/admin").denyAll()  // deny any acess to this endpoint used when we have a page that is under maintenance
+                           // .requestMatchers("/contact").permitAll()   // allow user to access the landing page without authentification
+                           .requestMatchers("/api/notes/**").permitAll()  // allow any endpoint start with public
+                           // .requestMatchers("/admin").denyAll()  // deny any acess to this endpoint used when we have a page that is under maintenance
                             .anyRequest().authenticated());// any request must be authentificated
+                            http.csrf(AbstractHttpConfigurer::disable);
 
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
@@ -28,3 +40,4 @@ public class SecurityConfig {
         return (SecurityFilterChain)http.build();
     }
 }
+*/
